10ee7276044c306797341fa3a643c400860658a0
jacki <2788307@clases.edu.sv>
2024-07-30 22:23:26 -0600

    Agregar archivos de proyecto.

---
+++ TrianguloAreaCalcular/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TrianguloAreaCalcular
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
